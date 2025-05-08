ae22b34da3ae9b0fb70ae80cf9e6b789cdf5e35f
Luyao-cube <3210198370@qq.com>
2025-05-08 20:36:33 +0800

    RESULT项目

---
+++ Result.java
@@ -0,0 +1,40 @@
+package com.example.demo.enaity;
+
+public class Result {
+    private int code;
+    private String message;
+    public Object data;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Result{" +
+                "code=" + code +
+                ", message='" + message + '\'' +
+                ", data=" + data +
+                '}';
+    }
+}
